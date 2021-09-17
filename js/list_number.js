24fb015a23fd102f7952f05637efeaf0fb227a3d
Alex123-G <alexgallozo521@gmial.com>
2021-09-17 17:13:46 -0500

    Boton restablecer en lista por nombre

--- js/list_number.js
+++ js/list_number.js
@@ -1,6 +1,12 @@
 const btn_action = document.getElementById("btn_action");
 
-const callApi = () => {};
+const callApi = async pokeon_id => {
+	const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeon_id}
+`);
+	const info = await url.json();
+	const data = info;
+	console.log(data);
+};
 
 const readData = () => {};
 
@@ -10,8 +16,8 @@ btn_action.addEventListener(`click`, () => {
 	if (num_min > num_max) {
 		alert("El primer número no puede ser mayor al segundo.");
 	} else {
-		// LLamar a la funcion
+		for (index = num_min; index <= num_max; index++) {
+			callApi(index);
+		}
 	}
-	console.log(num_min);
-	console.log(num_max);
 });
